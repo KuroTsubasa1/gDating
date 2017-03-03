8effa1992df27f3bb3156c61b880cb42cf0da857
Lasse Harm <lasseharm@web.de>
2017-03-03 17:17:27 +0100

    idk

--- src/app.py
+++ src/app.py
@@ -19,12 +19,13 @@ try:
     cnx = mysql.connector.connect(user='root', password='GEkyX4KFcZH!',
                                  host='127.0.0.1',
                                  database='testDatabase')
+    print("SQL : All Good")
     cursor = cnx.cursor()
 except mysql.connector.Error as err:
   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
+    print("SQL : Something is wrong with your user name or password")
   elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
+    print("SQL : Database does not exist")
   else:
     print(err)
 else:
