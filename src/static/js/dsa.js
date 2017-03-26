db5e09c338b482ed155fd25bccd38a7d17bd833d
Fynn Kröger <kroeger.fynn@web.de>
2017-03-26 19:16:44 +0200

    Dropbox angepasst für die Erfahrungsauswahl
    Die Dropbox mit den Erfahrungsgraden befüllt nun die textbox daneben mit
    dem ausgeschriebenen Wert

--- src/static/js/dsa.js
+++ src/static/js/dsa.js
@@ -8,68 +8,107 @@ var GE = 8;
 var KO = 8;
 var KK = 8;
 
-function onClickMen() {
-    var answerP11 = "1";
-    showGender(answerP11);
+function onClickUnerfahren() {
+    var LevelId = "1";
+    showLevel(LevelId);
 }
 
-function onClickWomen() {
-    var answerP11 = "2";
-    showGender(answerP11);
+function onClickDurchschnitt() {
+    var LevelId = "2";
+    showLevel(LevelId);
 }
 
-function onClickOther() {
-    var answerP11 = "3";
-    showGender(answerP11);
+function onClickErfahren() {
+    var LevelId = "3";
+    showLevel(LevelId);
 }
 
-function onClickLast() {
-    var answerP11 = "4"
-    showGender(answerP11)
+function onClickKompetent() {
+    var LevelId = "4"
+    showLevel(LevelId)
 }
 
-function showGender(answerP11) {
-    $('#displayAnswer1').val(answerP11);
+function onClickMeister() {
+    var LevelId = "5"
+    showLevel(LevelId)
 }
 
-function deterLep(gwLep,KO) {
-    return gwLep + (KO*2);
+function onClickBilliant() {
+    var LevelId = "6"
+    showLevel(LevelId)
 }
 
-function deterAusw(GE){
-return GE/2;
+function onClickLegend() {
+    var LevelId = "7"
+    showLevel(LevelId)
 }
 
-function deterIni(MU,GE){
-return (MU+GE)/2;
+function showLevel(LevelId) {
+    var LevelText = "";
+    switch (LevelId) {
+        case "1":
+            LevelText = "Unerfahren";
+            break;
+        case "2":
+            LevelText = "Durchschnittlich";
+            break;
+        case "3":
+            LevelText = "Erfahren";
+            break;
+        case "4":
+            LevelText = "Kompetent";
+            break;
+        case "5":
+            LevelText = "Meisterlich";
+            break;
+        case "6":
+            LevelText = "Brilliant";
+            break;
+        case "7":
+            LevelText = "Legendär";
+            break;
+    }
+    $('#displayAnswer1').val(LevelText);
+}
+
+function deterLep(gwLep, KO) {
+    return gwLep + (KO * 2);
+}
+
+function deterAusw(GE) {
+    return GE / 2;
+}
+
+function deterIni(MU, GE) {
+    return (MU + GE) / 2;
 }
 
 // Gibt den Astralpunktewert an, braucht die Leiteigenschaft der Zauberertradition an
 function deterAsp(LE) {
-    return 20+LE;
+    return 20 + LE;
 }
 
 // Gibt den Karmawert an, braucht die Leiteigenschaft der Geweihtentradition
 function deterKsp(LE) {
-    return 20+LE;
+    return 20 + LE;
 }
 
 // Berechnet die Seelenkraft aus Grundwert der Spezies für Seelenkraft, Mut, Klugheit, Intuition
-function deterSK(gwSK,MU,KL,IN) {
-    return (MU+KL+IN/6)+ gwSK;
+function deterSK(gwSK, MU, KL, IN) {
+    return (MU + KL + IN / 6) + gwSK;
 }
 
 // Berechnet die Zähigkeit aus Grundwert der Spezies für Zähigkeit, Kondition, Körperkraft
-function deterZK(gwZK,KO,KK) {
-    return ((KO+KO+KK)/6) + gwZK;
+function deterZK(gwZK, KO, KK) {
+    return ((KO + KO + KK) / 6) + gwZK;
 }
 
 var fruits = ['Parameter1', 'Parameter2', 'Parameter3'];
 var widget = new AutoComplete('search_bar', fruits);
 
-  $('#showVal').on('click', function() {
+$('#showVal').on('click', function () {
     console.log("Current value of the search bar:");
     var search_barTest = widget.getValue();
     search_barTest = search_barTest[0][0].value;
     console.log(search_barTest);
-  });
+});
